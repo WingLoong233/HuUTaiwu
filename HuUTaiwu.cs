743a33dcbb2776b186ade3f06f77772d2ae374c1
Yang Yuxuan <1659917134@qq.com>
2024-09-18 19:51:25 +0800

    Init repo.

---
+++ HuUTaiwu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using TaiwuModdingLib.Core.Plugin;
+
+namespace HuUTaiwu
+{
+    [PluginConfig("HuUTaiwu", "A.E.I.O.U", "1.0.0")]
+    public class HuUTaiwu : TaiwuRemakePlugin
+    {
+        public override void Initialize()
+        {
+            // Debug.Print("HuUTaiwu Init");
+
+        }
+
+        public override void Dispose()
+        {
+
+        }
+
+    }
+}
